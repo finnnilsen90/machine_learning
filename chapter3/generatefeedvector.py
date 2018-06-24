dec66dd3876da0e50b6603b881913c4fac488fb6
Finn Nilsen <finnnilsen@Finns-MacBook-Pro.local>
2018-06-24 00:38:14 -0700

    clustering

--- chapter3/generatefeedvector.py
+++ chapter3/generatefeedvector.py
@@ -17,6 +17,7 @@ def getwordcounts(url):
         for word in words:
             wc.setdefault(word,0)
             wc[word]+=1
+    # print(getattr(d.feed, 'title', 'Unknown title')+' => '+str(wc))
     return getattr(d.feed, 'title', 'Unknown title'), wc
     # return d.feed.title,wc
 
@@ -54,44 +55,12 @@ out.write('\n')
 for blog,wc in wordcounts.items():
     #deal with unicode outside the ascii range
     blog = blog.encode('ascii','ignore')
+    blog = blog.decode('utf-8')
 
-    out.write(str(blog))
+    out.write(blog)
     for word in wordlist:
-        if word in wc: out.write('\t%d' % wc[word])
+        if word in wc: 
+            out.write('\t%d' % wc[word])
+        else:
+            out.write('\t0')
     out.write('\n')
-
-
-# def readfile(filname):
-#     lines=[line for line in file(filname)]
-
-#     # First line is the column titles
-#     colnames=lines[0].strip().split('\t')[1:]
-#     rownames=[]
-#     data=[]
-#     for line in lines[1:]:
-#         p=line.strip().split('\t')
-#         # First column in each row is the rowname
-#         rownames.append(p[0])
-#         # The data for this row is the remainder of the row
-#         data.append([float(x) for x in p[1:]])
-#     return rownames,colnames,data
-
-# from math import sqrt
-# def person(v1,v2):
-#     # Simple sums
-#     sum1=sum(v1)
-#     sum2=sum(v2)
-
-#     # Sum of the squares
-#     sum1Sq=sum([pow(v,2) for v in v1])
-#     sum2Sq=sum([pow(v,2) for v in v2])
-
-#     # Sum of the products
-#     pSum=sum([v1[i]*v2[i] for i in range(len(v1))])
-
-#     # Calculate r (Pearson score)
-#     num=pSum-(sum1*sum2/len(v1))
-#     den=sqrt((sumSq-pow(sum1,2)/len(v1))*(sum2Sq-pow(sum2,2)/len(v1)))
-#     if den==0: return 0
-
-#     return 1.0-num/den
