aed2feafb631288f21a04045e06cdc30b3abd10f
Finn Nilsen <finnnilsen@Finns-MacBook-Pro.local>
2018-07-03 22:33:29 -0700

    chapter 3 final

--- chapter3/clusters.py
+++ chapter3/clusters.py
@@ -219,3 +219,76 @@ def kcluster(rows,distance=pearson,k=4):
                 clusters[i]=avgs
 
     return bestmatches
+
+def tanimoto(v1,v2):
+    c1,c2,shr=0,0,0
+
+    for i in range(len(v1)):
+        if v1[i]!=0: c1+=1 # in v1
+        if v2[i]!=0: c2+=1 # in v2
+        if v1[i]!=0 and v2[i]!=0: shr+=1 # in both
+
+    return 1.0-(float(shr)/(c1+c2-shr))
+
+def scaledown(data,distance=pearson,rate=0.01):
+    n=len(data)
+
+    # The real distances between every pair of items
+    realdist=[[distance(data[i],data[j]) for j in range(n)]
+            for i in range(0,n)]
+    
+    outersum=0.0
+
+    # Randomly initialize the starting points of the locations in 2D
+    loc=[[random.random(),random.random()] for i in range(n)]
+    fakedist=[[0.0 for j in range(n)] for i in range(n)]
+
+    lasterror=None 
+    for m in range(0,1000):
+        # Find rojected distances
+        for i in range(n):
+            for j in range(n):
+                fakedist[i][j]=sqrt(sum([pow(loc[i][x]-loc[j][x],2)
+                                for x in range(len(loc[i]))]))
+
+        # Move points
+        grad=[[0.0,0.0] for i in range(n)]
+
+        totalerror=0
+        for k in range(n):
+            for j in range(n):
+                if j==k: continue
+                # The error is percent difference between the distances
+                try:
+                    errorterm=(fakedist[j][k]-realdist[j][k])/realdist[j][k]
+                except ZeroDivisionError:
+                    errorterm=0
+
+                # Each point needs to be moved away from or towards the other
+                # point in proportion to how much error it has
+                grad[k][0]+=((loc[k][0]-loc[j][0])/fakedist[j][k])*errorterm
+                grad[k][0]+=((loc[k][1]-loc[j][1])/fakedist[j][k])*errorterm
+
+                # keep track of the total error
+                totalerror+=abs(errorterm)
+        print(totalerror)
+
+        # If the answer got worse by mocing the points, we are done
+        if lasterror and lasterror<totalerror: break
+        lasterror=totalerror
+
+        # Move each of the ponts by the learning rate times the gradient
+        for k in range(n):
+            loc[k][0]-=rate*grad[k][0]
+            loc[k][1]-=rate*grad[k][1]
+
+    return loc
+
+def draw2d(data,labels,jpeg='mds2d.jpg'):
+    img=Image.new('RGB',(2000,2000),(255,255,255))
+    draw=ImageDraw.Draw(img)
+    for i in range(len(data)):
+        x=(data[i][0]+0.5)*1000
+        y=(data[i][1]+0.5)*1000
+        draw.text((x,y),labels[i],(0,0,0))
+    img.save(jpeg,'JPEG')
