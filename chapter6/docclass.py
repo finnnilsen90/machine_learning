ae481c8e7334922fa552dd7c8d07137b8233cf8c
Finn Nilsen <finnnilsen@Finns-MacBook-Pro.local>
2018-09-19 00:26:36 -0700

    fisherclassifier

--- chapter6/docclass.py
+++ chapter6/docclass.py
@@ -135,6 +135,30 @@ class naivebayes(classifier):
 # freqsum = Sum of Pr(feature | category) for all the categories
 # cprob = clf/(clf+nclf)
 class fisherclassifier(classifier):
+
+    def __init__(self,getfeatures):
+        classifier.__init__(self,getfeatures)
+        self.minimums={}
+
+    def setminimum(self,cat,min):
+        self.minimums[cat]=min
+
+    def getminimum(self,cat):
+        if cat not in self.minimums: return 0
+        return self.minimums[cat]
+
+    def classify(self,item,default=None):
+        # Loop through looking for the best result
+        best=default
+        max=0.0
+        for c in self.categories():
+            p=self.fisherprob(item,c)
+            # Make sure it exceeds its minimum
+            if p>self.getminimum(c) and p>max:
+                best=c
+                max=p
+        return best
+
     def cprob(self,f,cat):
         # The frequency of this feature in this category
         clf=self.fprob(f,cat)
@@ -147,6 +171,28 @@ class fisherclassifier(classifier):
         p=clf/(freqsum)
 
         return p
+
+    def fisherprob(self,item,cat):
+       
+        # Multiply all the probabilities together
+        p=1
+        features=self.getfeatures(item)
+        for f in features:
+            p*=(self.weightedprob(f,cat,self.cprob))
+
+        # Take the natural log and multiply by -2
+        fscore=-2*math.log(p)
+
+        # Use teh inverse chi2 function to get the probability
+        return self.invchi2(fscore,len(features)*2)
+
+    def invchi2(self,chi,df):
+        m = chi / 2.0
+        sum = term = math.exp(-m)
+        for i in range(1,df//2):
+            term *= m / i
+            sum += term
+        return min(sum, 1.0)
 
 def sampletrain(cl):
     cl.train('Nobody owns the water','good')
