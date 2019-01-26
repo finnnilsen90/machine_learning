be3f1d7c8c7ae05321832ebcb5038a6d1afde0dc
Finn Nilsen <finnnilsen@finns-mbp.home>
2019-01-26 15:28:45 -0800

    chapter 8 cross validation

--- chapter8/numpredict.py
+++ chapter8/numpredict.py
@@ -64,3 +64,67 @@ def knnestimate(data,vec1,k=5):
         avg+=data[idx]['result']
     avg=avg/k 
     return avg 
+
+#### WEIGHTED NEIGHBORS ####
+
+## INVERSE FUNCTION ##
+
+def inverseweight(dist,num=1.0,const=0.1):
+    return num/(dist+const)
+
+## SUBTRACTION FUNCTION ##
+
+def subtractweight(dist,const=1.0):
+    if dist>const:
+        return 0
+    else:
+        return const-dist
+
+## GUASSIAN FUNCTION ##
+
+def gaussian(dist,sigma=10.0):
+    return math.e**(-dist**2/(2*sigma**2))
+
+#### WEIGHTEDD KNN ####
+
+def weightedknn(data,vec1,k=5,weightf=gaussian):
+    # Get distances
+    dlist=getdistances(data,vec1)
+    avg=0.0
+    totalweight=0.0
+
+    # Get weighted average 
+    for i in range(k):
+        dist=dlist[i][0]
+        idx=dlist[i][1]
+        weight=weightf(dist)
+        avg+=weight*data[idx]['result']
+        totalweight+=weight
+    avg=avg/totalweight
+    return avg
+
+#### CROSS-VALIDATION ####
+
+def dividedata(data,test=0.05):
+    trainset=[]
+    testset=[]
+    for row in data:
+        if random()<test:
+            testset.append(row)
+        else:
+            trainset.append(row)
+    return trainset,testset
+
+def testalgorithim(algf,trainset,testset):
+    error=0.0
+    for row in testset:
+        guess=algf(trainset,row['input'])
+        error+=(row['result']-guess)**2
+    return error/len(testset)
+
+def crossvalidate(algf,data,trials=100,test=0.05):
+    error=0.0
+    for i in range(trials):
+        trainset,testset=dividedata(data,test)
+        error+=testalgorithim(algf,trainset,testset)
+    return error/trials
