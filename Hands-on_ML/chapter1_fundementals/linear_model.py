d229ff19e818f45e91fabbe1f94bcc14c5c2fb9f
Finn Nilsen <finnnilsen@finns-mbp.home>
2019-04-21 11:45:09 -0700

    mimis commit

---
+++ Hands-on_ML/chapter1_fundementals/linear_model.py
@@ -0,0 +1,40 @@
+import matplotlib
+import matplotlib.pyplot as plt 
+import numpy as np 
+import pandas as pd 
+import sklearn.linear_model
+
+# Load data
+oecd_bli = pd.read_csv('oecd_bli_2015.csv', thousands=',')
+gdp_per_capita = pd.read_csv('gdp_per_capita.csv', thousands=',',delimiter='\t',encoding='latin1', na_values='n/a')
+
+# Prepare country stats function
+def prepare_country_stats(oecd_bli, gdp_per_capita):
+    oecd_bli = oecd_bli[oecd_bli["INEQUALITY"]=="TOT"]
+    oecd_bli = oecd_bli.pivot(index="Country", columns="Indicator", values="Value")
+    gdp_per_capita.rename(columns={"2015": "GDP per capita"}, inplace=True)
+    gdp_per_capita.set_index("Country", inplace=True)
+    full_country_stats = pd.merge(left=oecd_bli, right=gdp_per_capita,left_index=True, right_index=True)
+    full_country_stats.sort_values(by="GDP per capita", inplace=True)
+    remove_indices = [0, 1, 6, 8, 33, 34, 35]
+    keep_indices = list(set(range(36)) - set(remove_indices))
+    return full_country_stats[["GDP per capita", 'Life satisfaction']].iloc[keep_indices]
+
+# Prepare teh data
+country_stats = prepare_country_stats(oecd_bli, gdp_per_capita)
+x = np.c_[country_stats['GDP per capita']]
+y = np.c_[country_stats['Life satisfaction']]
+
+# Visualize the data
+country_stats.plot(kind='scatter', x='GDP per capita', y='Life satisfaction')
+plt.show()
+
+# Select a linear model
+model = sklearn.linear_model.LindearRegression()
+
+# Train the model
+model.fit(x,y)
+
+# Make a prediction for Cyprus
+X_new = [[22587]] # Cyprus' GDP per capita
+print(model.predict(X_new)) # outputs [[5.962338]]
